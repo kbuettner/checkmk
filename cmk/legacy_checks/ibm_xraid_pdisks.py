bf9f4a89c19cef9fe8a8e5abbb06c6ab11bc172f
Dennis Schmalacker <dennis.schmalacker@checkmk.com>
2026-02-27 17:45:29 +0100

    Legacy check migration: ibm_xraid_pdisks I
    Change-Id: I3e0a15fa3e680fe5cd0dd05349fddbf1247260f9

--- cmk/legacy_checks/ibm_xraid_pdisks.py
+++ cmk/legacy_checks/ibm_xraid_pdisks.py
@@ -3,52 +3,70 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
 
 # Parse the snmp data from the IBM agent.
 # must be cautius since a failed disk makes the snmp output
 # shift and gives false info for "slot_id"
 
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import all_of, any_of, equals, exists, SNMPTree
+from cmk.agent_based.v2 import (
+    all_of,
+    any_of,
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    equals,
+    exists,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 
-check_info = {}
+Section = dict[str, tuple[int, str, str, str, str]]
 
 
-def parse_ibm_xraid_pdisks(info):
-    data = {}
+def parse_ibm_xraid_pdisks(info: StringTable) -> Section:
+    data: Section = {}
     for line in info:
         slot_id, disk_id, disk_type, disk_state, slot_desc = line
         if "slot" in slot_desc.lower():
-            slot_id = int(slot_desc.split(", ")[-1][-1])
+            slot_id_int = int(slot_desc.split(", ")[-1][-1])
             enc_id = int(slot_desc.split(", ")[-2][-1])
             hba_id = int(slot_desc.split(", ")[-3][-1])
-            disk_path = ("%d/%d/%d") % (hba_id, enc_id, slot_id)
-            data.update({disk_path: (slot_id, disk_id, disk_type, disk_state, slot_desc)})
+            disk_path = f"{hba_id}/{enc_id}/{slot_id_int}"
+            data[disk_path] = (slot_id_int, disk_id, disk_type, disk_state, slot_desc)
     return data
 
 
-def discover_ibm_xraid_pdisks(section):
-    return [(disk_id, None) for disk_id in section]
+def discover_ibm_xraid_pdisks(section: Section) -> DiscoveryResult:
+    yield from (Service(item=disk_id) for disk_id in section)
 
 
-def check_ibm_xraid_pdisks(item, _no_params, section):
+def check_ibm_xraid_pdisks(item: str, section: Section) -> CheckResult:
     for disk_path, disk_entry in section.items():
         if disk_path == item:
             _slot_label, _disk_id, _disk_type, disk_state, slot_desc = disk_entry
             if disk_state == "3":
-                return (0, "Disk is active" + " [%s]" % slot_desc)
+                yield Result(state=State.OK, summary=f"Disk is active [{slot_desc}]")
+                return
             if disk_state == "4":
-                return (1, "Disk is rebuilding" + " [%s]" % slot_desc)
+                yield Result(state=State.WARN, summary=f"Disk is rebuilding [{slot_desc}]")
+                return
             if disk_state == "5":
-                return (2, "Disk is dead" + " [%s]" % slot_desc)
+                yield Result(state=State.CRIT, summary=f"Disk is dead [{slot_desc}]")
+                return
 
-    return (2, "disk is missing")  # + " [%s]" % data[item][4])
+    yield Result(state=State.CRIT, summary="disk is missing")
 
 
-check_info["ibm_xraid_pdisks"] = LegacyCheckDefinition(
+# there is no information about the ext mib in the right place
+# (at least on windows)
+# this means the check has to fetch a specific oid. Limit this
+# effect to relevant systems to lessen useless scanning.,
+snmp_section_ibm_xraid_pdisks = SimpleSNMPSection(
     name="ibm_xraid_pdisks",
     detect=all_of(
         any_of(
@@ -61,11 +79,12 @@ check_info["ibm_xraid_pdisks"] = LegacyCheckDefinition(
         oids=["503.1.1.4", "400.1.1.1", "400.1.1.5", "400.1.1.11", "400.1.1.12"],
     ),
     parse_function=parse_ibm_xraid_pdisks,
+)
+
+
+check_plugin_ibm_xraid_pdisks = CheckPlugin(
+    name="ibm_xraid_pdisks",
     service_name="RAID PDisk %s",
     discovery_function=discover_ibm_xraid_pdisks,
-    # there is no information about the ext mib in the right place
-    # (at least on windows)
-    # this means the check has to fetch a specific oid. Limit this
-    # effect to relevant systems to lessen useless scanning.,
     check_function=check_ibm_xraid_pdisks,
 )
