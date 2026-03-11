235cdaeab7eee21eb30f737078016c4c054877b1
Simon Jess <simon.jess@checkmk.com>
2026-03-11 20:31:57 +0100

    Fix adding graph to custom graph or collection
    Change-Id: Id64dc78ad4e1b496ac65e9c0d5ccb006aa1bfe04

--- packages/cmk-frontend/src/js/modules/popup_menu.ts
+++ packages/cmk-frontend/src/js/modules/popup_menu.ts
@@ -528,7 +528,7 @@ export function pagetype_add_to_container(page_type: string, page_name: string) 
 
 export function graph_export(page: string) {
   const request = {
-    specification: active_popup.data![2]['definition']['specification'],
+    specification: active_popup.data![2]['graph_recipe']['specification'],
     data_range: active_popup.data![2]['data_range']
   }
   location.href = page + '.py?request=' + encodeURIComponent(JSON.stringify(request))
