b8644c9d21d4cfca1a28209ea21af1f9f7a9cc38
Andreas Umbreit <andreas.umbreit@checkmk.com>
2026-03-13 20:56:11 +0100

    Fix bazel sanity check again
    Otherwise this would break on next Python script under .claude again.
    
    .claude exclusion was already set previously, but was dropped during a
    refactoring.
    
    Change-Id: I975e84facd35dc1ecc63c89fd8f3e78f3d032a3c

--- scripts/in_bazel.sh
+++ scripts/in_bazel.sh
@@ -11,7 +11,7 @@ limit=${3:-0}
 
 out=$(
     comm -23 \
-        <(git ls-files "*\.$ext" ":!doc/*" | sort) \
+        <(git ls-files "*\.$ext" ":!doc/*" ":!.claude/*" | sort) \
         <(
             bazel cquery '
             kind("source file", deps(kind("'"$kind"'", //...)))
