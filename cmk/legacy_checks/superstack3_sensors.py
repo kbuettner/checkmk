b69bb32924af84c1ace4acd01a8fdbbe264f82f7
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-03-14 23:58:40 +0100

    Legacy check migration: superstack3_sensors I
    Change-Id: I9236a7c4c2b1e55605119aba2578be04ebe12feb

--- cmk/legacy_checks/superstack3_sensors.py
+++ cmk/legacy_checks/superstack3_sensors.py
@@ -3,35 +3,43 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
+
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    contains,
+    DiscoveryResult,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import contains, SNMPTree, StringTable
-
-check_info = {}
+def discover_superstack3_sensors(section: StringTable) -> DiscoveryResult:
+    yield from (Service(item=line[0]) for line in section if line[1] != "not present")
 
 
-def discover_superstack3_sensors(info):
-    return [(line[0], None) for line in info if line[1] != "not present"]
-
-
-def check_superstack3_sensors(item, params, info):
-    for name, state in info:
+def check_superstack3_sensors(item: str, section: StringTable) -> CheckResult:
+    for name, state in section:
         if name == item:
             if state == "failure":
-                return (2, "status is %s" % state)
-            if state == "operational":
-                return (0, "status is %s" % state)
-            return (1, "status is %s" % state)
-    return (3, "UNKOWN - sensor not found")
+                yield Result(state=State.CRIT, summary=f"status is {state}")
+            elif state == "operational":
+                yield Result(state=State.OK, summary=f"status is {state}")
+            else:
+                yield Result(state=State.WARN, summary=f"status is {state}")
+            return
+    yield Result(state=State.UNKNOWN, summary="UNKOWN - sensor not found")
 
 
 def parse_superstack3_sensors(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["superstack3_sensors"] = LegacyCheckDefinition(
+snmp_section_superstack3_sensors = SimpleSNMPSection(
     name="superstack3_sensors",
     parse_function=parse_superstack3_sensors,
     detect=contains(".1.3.6.1.2.1.1.1.0", "3com superstack 3"),
@@ -39,6 +47,10 @@ check_info["superstack3_sensors"] = LegacyCheckDefinition(
         base=".1.3.6.1.4.1.43.43.1.1",
         oids=["7", "10"],
     ),
+)
+
+check_plugin_superstack3_sensors = CheckPlugin(
+    name="superstack3_sensors",
     service_name="%s",
     discovery_function=discover_superstack3_sensors,
     check_function=check_superstack3_sensors,
