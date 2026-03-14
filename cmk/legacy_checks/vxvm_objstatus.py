4499900251607c70442735cfd0c3b1cf9e25e224
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-03-14 22:32:20 +0100

    Legacy check migration: vxvm_objstatus I
    Change-Id: Iebc8985911d7c90697eb572e83705ee62dc0b7f0

--- cmk/legacy_checks/vxvm_objstatus.py
+++ cmk/legacy_checks/vxvm_objstatus.py
@@ -3,9 +3,6 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-call"
-# mypy: disable-error-code="no-untyped-def"
-
 # Example output:
 # <<<vxvm_objstatus>>>
 # v datadg lalavol CLEAN DISABLED
@@ -15,61 +12,67 @@
 # v testgroup oravol-L02 ACTIVE ENABLED
 
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import StringTable
+from cmk.agent_based.v2 import (
+    AgentSection,
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Result,
+    Service,
+    State,
+    StringTable,
+)
 
-check_info = {}
 
-
-def vxvm_objstatus_disks(info):
-    groups = {}
-    found_groups = []
+def vxvm_objstatus_disks(info: StringTable) -> dict[str, list[tuple[str, str, str]]]:
+    groups: dict[str, list[tuple[str, str, str]]] = {}
     for dg_type, dg_name, name, admin_state, kernel_state in info:
         if dg_type == "v":
-            if dg_name not in found_groups:
-                groups[dg_name] = [(name, admin_state, kernel_state)]
-                found_groups.append(dg_name)
-            else:
-                groups[dg_name].append((name, admin_state, kernel_state))
+            groups.setdefault(dg_name, []).append((name, admin_state, kernel_state))
     return groups
 
 
-def discover_vxvm_objstatus(info):
-    return [(k, {}) for k in vxvm_objstatus_disks(info)]
+def discover_vxvm_objstatus(section: StringTable) -> DiscoveryResult:
+    yield from (Service(item=k) for k in vxvm_objstatus_disks(section))
 
 
-def check_vxvm_objstatus(item, _no_params, info):
-    groups = vxvm_objstatus_disks(info)
+def check_vxvm_objstatus(item: str, section: StringTable) -> CheckResult:
+    groups = vxvm_objstatus_disks(section)
     volumes = groups.get(item)
     if volumes is not None:
-        state = 0
+        state = State.OK
         messages = []
         for volume, admin_state, kernel_state in volumes:
             text = []
             error = False
             if admin_state not in {"CLEAN", "ACTIVE"}:
-                state = 2
+                state = State.CRIT
                 text.append(f"{volume}: Admin state is {admin_state}(!!)")
                 error = True
             if kernel_state not in {"ENABLED", "DISABLED"}:
-                state = 2
+                state = State.CRIT
                 text.append(f"{volume}: Kernel state is {kernel_state}(!!)")
                 error = True
             if error is False:
-                text = ["%s: OK" % volume]
+                text = [f"{volume}: OK"]
             messages.append(", ".join(text))
-        return (state, ", ".join(messages))
+        yield Result(state=state, summary=", ".join(messages))
+        return
 
-    return (2, "Group not found")
+    yield Result(state=State.CRIT, summary="Group not found")
 
 
 def parse_vxvm_objstatus(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["vxvm_objstatus"] = LegacyCheckDefinition(
+agent_section_vxvm_objstatus = AgentSection(
     name="vxvm_objstatus",
     parse_function=parse_vxvm_objstatus,
+)
+
+check_plugin_vxvm_objstatus = CheckPlugin(
+    name="vxvm_objstatus",
     service_name="VXVM objstatus %s",
     discovery_function=discover_vxvm_objstatus,
     check_function=check_vxvm_objstatus,
