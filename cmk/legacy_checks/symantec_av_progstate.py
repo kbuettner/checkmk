75ad97e7d006a0ccee8ce74026c5903d2885c9a5
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-03-14 23:45:57 +0100

    Legacy check migration: symantec_av_progstate I
    Change-Id: I2f8cc3e5b98cba904809201f35243c19e9cd9ad8

--- cmk/legacy_checks/symantec_av_progstate.py
+++ cmk/legacy_checks/symantec_av_progstate.py
@@ -3,34 +3,43 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
 # Example output from agent:
 
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import StringTable
+from cmk.agent_based.v2 import (
+    AgentSection,
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Result,
+    Service,
+    State,
+    StringTable,
+)
 
-check_info = {}
+
+def discover_symantec_av_progstate(section: StringTable) -> DiscoveryResult:
+    yield Service()
 
 
-def discover_symantec_av_progstate(info):
-    return [(None, None)]
-
-
-def check_symantec_av_progstate(_no_item, _no_params, info):
-    if info[0][0].lower() != "enabled":
-        return 2, "Program Status is " + info[0][0]
-    return 0, "Program enabled"
+def check_symantec_av_progstate(section: StringTable) -> CheckResult:
+    if section[0][0].lower() != "enabled":
+        yield Result(state=State.CRIT, summary=f"Program Status is {section[0][0]}")
+        return
+    yield Result(state=State.OK, summary="Program enabled")
 
 
 def parse_symantec_av_progstate(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["symantec_av_progstate"] = LegacyCheckDefinition(
+agent_section_symantec_av_progstate = AgentSection(
     name="symantec_av_progstate",
     parse_function=parse_symantec_av_progstate,
+)
+
+check_plugin_symantec_av_progstate = CheckPlugin(
+    name="symantec_av_progstate",
     service_name="AV Program Status",
     discovery_function=discover_symantec_av_progstate,
     check_function=check_symantec_av_progstate,
