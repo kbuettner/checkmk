6010b8d9ce2ebaec04df0172d237e1fdfe5a3ed2
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-03-14 23:00:32 +0100

    Legacy check migration: unitrends_replication I
    Change-Id: I35fc4cd450049b01b7f88b08742ec0a9d4acd763

--- cmk/legacy_checks/unitrends_replication.py
+++ cmk/legacy_checks/unitrends_replication.py
@@ -3,47 +3,55 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
+
+from cmk.agent_based.v2 import (
+    AgentSection,
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Result,
+    Service,
+    State,
+    StringTable,
+)
 
 
-# mypy: disable-error-code="var-annotated"
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import StringTable
-
-check_info = {}
-
-
-def discover_unitrends_replication(info):
-    inventory = []
-    for _application, _result, _complete, target, _instance in info:
-        if target not in [x[0] for x in inventory]:
-            inventory.append((target, None))
-    return inventory
+def discover_unitrends_replication(section: StringTable) -> DiscoveryResult:
+    seen: set[str] = set()
+    for _application, _result, _complete, target, _instance in section:
+        if target not in seen:
+            seen.add(target)
+            yield Service(item=target)
 
 
-def check_unitrends_replication(item, _no_params, info):
+def check_unitrends_replication(item: str, section: StringTable) -> CheckResult:
     # this never gone be a blessed check :)
-    replications = [x for x in info if x[3] == item]
+    replications = [x for x in section if x[3] == item]
     if len(replications) == 0:
-        return 3, "No Entries found"
+        yield Result(state=State.UNKNOWN, summary="No Entries found")
+        return
     not_successfull = [x for x in replications if x[1] != "Success"]
     if len(not_successfull) == 0:
-        return 0, "All Replications in the last 24 hours Successfull"
+        yield Result(state=State.OK, summary="All Replications in the last 24 hours Successfull")
+        return
     messages = []
     for _application, result, _complete, target, instance in not_successfull:
         messages.append(f"Target: {target}, Result: {result}, Instance: {instance}  ")
     # TODO: Maybe a good place to use multiline output here
-    return 2, "Errors from the last 24 hours: " + "/ ".join(messages)
+    yield Result(state=State.CRIT, summary="Errors from the last 24 hours: " + "/ ".join(messages))
 
 
 def parse_unitrends_replication(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["unitrends_replication"] = LegacyCheckDefinition(
+agent_section_unitrends_replication = AgentSection(
     name="unitrends_replication",
     parse_function=parse_unitrends_replication,
+)
+
+check_plugin_unitrends_replication = CheckPlugin(
+    name="unitrends_replication",
     service_name="Replicaion %s",
     discovery_function=discover_unitrends_replication,
     check_function=check_unitrends_replication,
