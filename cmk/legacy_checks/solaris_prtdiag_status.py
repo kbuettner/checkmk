c43cdb807bdf4738d9b5a5f8767ff42107d3592c
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-03-15 15:34:24 +0100

    Legacy check migration: solaris_prtdiag_status I
    Change-Id: I1068e85f780b637b81ead46b5b5b1edef492ca1c

--- cmk/legacy_checks/solaris_prtdiag_status.py
+++ cmk/legacy_checks/solaris_prtdiag_status.py
@@ -3,47 +3,55 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
 # Example output from agent:
 # <<<solaris_prtdiag_status>>>
 # 0
 
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import StringTable
+from cmk.agent_based.v2 import (
+    AgentSection,
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Result,
+    Service,
+    State,
+    StringTable,
+)
 
-check_info = {}
+
+def discover_solaris_prtdiag_status(section: StringTable) -> DiscoveryResult:
+    if section:
+        yield Service()
 
 
-def discover_solaris_prtdiag_status(info):
-    if info:
-        return [(None, None)]
-    return []
-
-
-def check_solaris_prtdiag_status(_no_item, _no_params, info):
-    if not info:
-        return None
+def check_solaris_prtdiag_status(section: StringTable) -> CheckResult:
+    if not section:
+        return
 
     # 0 No failures or errors are detected in the system.
     # 1 Failures or errors are detected in the system.
-    if int(info[0][0]) == 0:
-        return 0, "No failures or errors are reported"
-    return (
-        2,
-        "Failures or errors are reported by the system. "
-        'Please check the output of "prtdiag -v" for details.',
-    )
+    if int(section[0][0]) == 0:
+        yield Result(state=State.OK, summary="No failures or errors are reported")
+    else:
+        yield Result(
+            state=State.CRIT,
+            summary="Failures or errors are reported by the system. "
+            'Please check the output of "prtdiag -v" for details.',
+        )
 
 
 def parse_solaris_prtdiag_status(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["solaris_prtdiag_status"] = LegacyCheckDefinition(
+agent_section_solaris_prtdiag_status = AgentSection(
     name="solaris_prtdiag_status",
     parse_function=parse_solaris_prtdiag_status,
+)
+
+check_plugin_solaris_prtdiag_status = CheckPlugin(
+    name="solaris_prtdiag_status",
     service_name="Hardware Overall State",
     discovery_function=discover_solaris_prtdiag_status,
     check_function=check_solaris_prtdiag_status,
