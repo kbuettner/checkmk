d3066c5698bc285b211895ec95b9c67e42c19add
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-03-15 15:35:38 +0100

    Legacy check migration: sensatronics_temp I
    Change-Id: I4ea45b33213bcc4075ad62656d2ee66dc3c4a60b

--- cmk/legacy_checks/sensatronics_temp.py
+++ cmk/legacy_checks/sensatronics_temp.py
@@ -3,15 +3,21 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="type-arg"
+from collections.abc import Mapping, Sequence
 
-from collections.abc import Iterable, Mapping
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import any_of, equals, SNMPTree
-from cmk.legacy_includes.temperature import check_temperature, TempParamType
-
-check_info = {}
+from cmk.agent_based.v2 import (
+    any_of,
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    equals,
+    get_value_store,
+    Service,
+    SNMPSection,
+    SNMPTree,
+    StringTable,
+)
+from cmk.plugins.lib.temperature import check_temperature, TempParamType
 
 Section = Mapping[str, float]
 
@@ -19,7 +25,7 @@ Section = Mapping[str, float]
 _TABLES = list(range(16))
 
 
-def parse_sensatronics_temp(string_table: list[list[list[str]]]) -> Section:
+def parse_sensatronics_temp(string_table: Sequence[StringTable]) -> Section:
     parsed = {}
     for oid, block in zip(_TABLES, string_table):
         for name, reading in block:
@@ -30,19 +36,7 @@ def parse_sensatronics_temp(string_table: list[list[list[str]]]) -> Section:
     return parsed
 
 
-def discover_sensatronics_temp(section: Section) -> Iterable[tuple[str, dict]]:
-    yield from ((item, {}) for item in section)
-
-
-def check_sensatronics_temp(
-    item: str, params: TempParamType, section: Section
-) -> Iterable[tuple[int, str, list]]:
-    if (reading := section.get(item)) is None:
-        return
-    yield check_temperature(reading, params, "sensatronics_temp_%s" % item)
-
-
-check_info["sensatronics_temp"] = LegacyCheckDefinition(
+snmp_section_sensatronics_temp = SNMPSection(
     name="sensatronics_temp",
     detect=any_of(equals(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.16174.1.1.1")),
     fetch=[
@@ -56,6 +50,26 @@ check_info["sensatronics_temp"] = LegacyCheckDefinition(
         for table in _TABLES
     ],
     parse_function=parse_sensatronics_temp,
+)
+
+
+def discover_sensatronics_temp(section: Section) -> DiscoveryResult:
+    yield from (Service(item=item) for item in section)
+
+
+def check_sensatronics_temp(item: str, params: TempParamType, section: Section) -> CheckResult:
+    if (reading := section.get(item)) is None:
+        return
+    yield from check_temperature(
+        reading=reading,
+        params=params,
+        unique_name=f"sensatronics_temp_{item}",
+        value_store=get_value_store(),
+    )
+
+
+check_plugin_sensatronics_temp = CheckPlugin(
+    name="sensatronics_temp",
     service_name="Temperature %s",
     discovery_function=discover_sensatronics_temp,
     check_function=check_sensatronics_temp,
