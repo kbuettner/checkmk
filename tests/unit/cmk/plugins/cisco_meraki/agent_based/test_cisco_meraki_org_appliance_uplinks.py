09947e889a51d83fe1a51c763d5f5da77cfd4481
Logan Connolly <logan.connolly@checkmk.com>
2026-03-16 09:51:27 +0100

    cisco_meraki: use enum values for test params
    Change-Id: Ib8d32a41cfb94a8e75dada385a351ff35804d22a

--- tests/unit/cmk/plugins/cisco_meraki/agent_based/test_cisco_meraki_org_appliance_uplinks.py
+++ tests/unit/cmk/plugins/cisco_meraki/agent_based/test_cisco_meraki_org_appliance_uplinks.py
@@ -42,7 +42,13 @@ def test_discover_appliance_uplinks() -> None:
 @pytest.fixture
 def params() -> CheckParams:
     return CheckParams(
-        status_map={"active": 0, "ready": 0, "connecting": 1, "not_connected": 1, "failed": 2},
+        status_map={
+            "active": State.OK.value,
+            "ready": State.OK.value,
+            "connecting": State.WARN.value,
+            "not_connected": State.WARN.value,
+            "failed": State.CRIT.value,
+        },
         show_traffic=True,
     )
 
