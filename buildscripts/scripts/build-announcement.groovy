2a72df9b8324570fe8d5f677b3141ba19423faa7
Jonas Scharpf <jonas.scharpf@checkmk.com>
2026-03-16 11:47:33 +0100

    Move build-announcement to k8s
    Requires https://review.lan.tribe29.com/c/checkmk_ci/+/131119
    
    CMK-31467
    
    Change-Id: Ic33534a71e756b835f055aa2e6c0b5b275e19b1c

--- buildscripts/scripts/build-announcement.groovy
+++ buildscripts/scripts/build-announcement.groovy
@@ -6,15 +6,17 @@
 // Artifacts will be consumed by bw-release.
 
 void main() {
-    stage("Clean workspace") {
-        dir("${checkout_dir}") {
-            // We don't want to fill up the workspace with old annoucement files
-            sh(script: "make clean");
-        }
-    }
-    stage("Build announcement") {
-        inside_container() {
-            dir("${checkout_dir}") {
+    def versioning = load("${checkout_dir}/buildscripts/scripts/utils/versioning.groovy");
+    def safe_branch_name = versioning.safe_branch_name();
+
+    dir("${checkout_dir}") {
+        inside_container_minimal(safe_branch_name: safe_branch_name) {
+            stage("Clean workspace") {
+                // We don't want to fill up the workspace with old annoucement files
+                sh(script: "make clean");
+            }
+
+            stage("Build announcement") {
                 def announce_file = sh(script: 'make print-CHECK_MK_ANNOUNCE_TAR_FILE', returnStdout: true).trim();
                 sh(script: "make announcement");
                 show_duration("archiveArtifacts") {
