70d190fc3bd424405a6fc7593c2307e67d5e671c
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-03-16 21:54:37 +0100

    Legacy check migration: cadvisor_df I
    Change-Id: I1c7566c4571c94e477f02dc13f24e4bf86725ba2

--- cmk/legacy_checks/cadvisor_df.py
+++ cmk/legacy_checks/cadvisor_df.py
@@ -3,25 +3,28 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-# mypy: disable-error-code="type-arg"
-
 
 import json
-from collections.abc import Iterable, Mapping
+from collections.abc import Mapping
 from typing import Any
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.legacy_includes.df import df_check_filesystem_single, FILESYSTEM_DEFAULT_PARAMS
-
-check_info = {}
+from cmk.agent_based.v2 import (
+    AgentSection,
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    get_value_store,
+    Service,
+    StringTable,
+)
+from cmk.plugins.lib.df import df_check_filesystem_single, FILESYSTEM_DEFAULT_PARAMS
 
 Section = Mapping[str, Any]
 
 
-def parse_cadvisor_df(string_table):
+def parse_cadvisor_df(string_table: StringTable) -> Section:
     diskstat_info = json.loads(string_table[0][0])
-    parsed = {}
+    parsed: dict[str, float] = {}
     for diskstat_name, diskstat_entries in diskstat_info.items():
         if len(diskstat_entries) != 1:
             continue
@@ -32,26 +35,37 @@ def parse_cadvisor_df(string_table):
     return parsed
 
 
-def discover_cadvisor_df(section: Section) -> Iterable[tuple[None, dict]]:
+def discover_cadvisor_df(section: Section) -> DiscoveryResult:
     if section:
-        yield None, {}
+        yield Service()
 
 
-def check_cadvisor_df(item, _params, parsed):
-    size_mb = parsed["df_size"] / 1024**2
-    avail_mb = size_mb - (parsed["df_used"] / 1024**2)
-    reserved_mb = 0
-    inodes_total = parsed["inodes_total"]
-    inodes_free = parsed["inodes_free"]
-    return df_check_filesystem_single(
-        item, size_mb, avail_mb, reserved_mb, inodes_total, inodes_free, FILESYSTEM_DEFAULT_PARAMS
+def check_cadvisor_df(params: Mapping[str, Any], section: Section) -> CheckResult:
+    size_mb = section["df_size"] / 1024**2
+    avail_mb = size_mb - (section["df_used"] / 1024**2)
+    inodes_total = section["inodes_total"]
+    inodes_free = section["inodes_free"]
+    yield from df_check_filesystem_single(
+        value_store=get_value_store(),
+        mountpoint="filesystem",
+        filesystem_size=size_mb,
+        free_space=avail_mb,
+        reserved_space=0,
+        inodes_total=inodes_total,
+        inodes_avail=inodes_free,
+        params=params,
     )
 
 
-check_info["cadvisor_df"] = LegacyCheckDefinition(
+agent_section_cadvisor_df = AgentSection(
     name="cadvisor_df",
     parse_function=parse_cadvisor_df,
+)
+
+check_plugin_cadvisor_df = CheckPlugin(
+    name="cadvisor_df",
     service_name="Filesystem",
     discovery_function=discover_cadvisor_df,
     check_function=check_cadvisor_df,
+    check_default_parameters=FILESYSTEM_DEFAULT_PARAMS,
 )
