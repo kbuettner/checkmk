84b3037cdff3cbcc2ad88b37717d871eaeb3afba
Logan Connolly <logan.connolly@checkmk.com>
2026-03-16 16:48:40 +0100

    cisco_meraki: add xfail tests for server side call
    CMK-32531
    
    Change-Id: I48b25fdd2c683ad983f860947e67577cca62a979

--- tests/unit/cmk/plugins/cisco_meraki/server_side_calls/test_meraki.py
+++ tests/unit/cmk/plugins/cisco_meraki/server_side_calls/test_meraki.py
@@ -137,6 +137,24 @@ HOST_CONFIG = HostConfig(
             ],
             id="Organisation IDs",
         ),
+        pytest.param(
+            {
+                "api_key": Secret(0),
+                "sections": [],
+                "orgs": [],
+            },
+            [
+                SpecialAgentCommand(
+                    command_arguments=[
+                        "testhost",
+                        "--apikey-id",
+                        Secret(0),
+                    ]
+                )
+            ],
+            id="Empty sequences are ignored",
+            marks=pytest.mark.xfail(reason="Only checked against None", strict=True),
+        ),
     ],
 )
 def test_argument_parsing(
