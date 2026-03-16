991aec1ad363d79aae4294b6cd6fe9f101a0f643
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-03-16 21:53:42 +0100

    Legacy check migration: orion_batterytest I
    Change-Id: Id62a324442f5358b4d85e278f98a472231035db5

--- cmk/legacy_checks/orion_batterytest.py
+++ cmk/legacy_checks/orion_batterytest.py
@@ -3,46 +3,26 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
 
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, startswith, StringTable
-
-check_info = {}
-
-
-def discover_orion_batterytest(info):
-    return [(None, {})]
-
-
-def check_orion_batterytest(item, params, info):
-    map_states = {
-        "1": (0, "none"),
-        "2": (2, "failed"),
-        "3": (1, "aborted"),
-        "4": (2, "load failure"),
-        "5": (0, "OK"),
-        "6": (1, "aborted manual"),
-        "7": (1, "aborted ev ctrl charge"),
-        "8": (1, "aborted inhibit ev"),
-    }
-
-    last_test_date, test_result = info[0]
-    if test_result != "1":
-        # dcBatteryTestResult:
-        # This parameter is valid only if there is a test result available.
-        state, state_readable = map_states.get(test_result, (3, "unknown[%s]" % test_result))
-        infotext = f"Last performed: {last_test_date}, Result: {state_readable}"
-        return state, infotext
-    return 0, "No test result available"
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    startswith,
+    State,
+    StringTable,
+)
 
 
 def parse_orion_batterytest(string_table: StringTable) -> StringTable | None:
     return string_table or None
 
 
-check_info["orion_batterytest"] = LegacyCheckDefinition(
+snmp_section_orion_batterytest = SimpleSNMPSection(
     name="orion_batterytest",
     parse_function=parse_orion_batterytest,
     detect=startswith(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.20246"),
@@ -50,6 +30,41 @@ check_info["orion_batterytest"] = LegacyCheckDefinition(
         base=".1.3.6.1.4.1.20246.2.3.1.1.1.2.5.2.2",
         oids=["1", "2"],
     ),
+)
+
+
+def discover_orion_batterytest(section: StringTable) -> DiscoveryResult:
+    yield Service()
+
+
+def check_orion_batterytest(section: StringTable) -> CheckResult:
+    map_states: dict[str, tuple[State, str]] = {
+        "1": (State.OK, "none"),
+        "2": (State.CRIT, "failed"),
+        "3": (State.WARN, "aborted"),
+        "4": (State.CRIT, "load failure"),
+        "5": (State.OK, "OK"),
+        "6": (State.WARN, "aborted manual"),
+        "7": (State.WARN, "aborted ev ctrl charge"),
+        "8": (State.WARN, "aborted inhibit ev"),
+    }
+
+    last_test_date, test_result = section[0]
+    if test_result != "1":
+        # dcBatteryTestResult:
+        # This parameter is valid only if there is a test result available.
+        state, state_readable = map_states.get(
+            test_result, (State.UNKNOWN, f"unknown[{test_result}]")
+        )
+        yield Result(
+            state=state, summary=f"Last performed: {last_test_date}, Result: {state_readable}"
+        )
+    else:
+        yield Result(state=State.OK, summary="No test result available")
+
+
+check_plugin_orion_batterytest = CheckPlugin(
+    name="orion_batterytest",
     service_name="Battery Test",
     discovery_function=discover_orion_batterytest,
     check_function=check_orion_batterytest,
