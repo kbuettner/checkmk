b970281099a0d2d568c92ab84909ec1be1de02aa
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-03-16 21:38:03 +0100

    Legacy check migration: heartbeat_rscstatus I
    Change-Id: Ib5984c65a0b8248cdbe49c7d031d40184093e525

--- cmk/legacy_checks/heartbeat_rscstatus.py
+++ cmk/legacy_checks/heartbeat_rscstatus.py
@@ -3,54 +3,69 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
 # <<<heartbeat_rscstatus>>>
 # all
 #
 # Status can be "local", "foreign", "all" or "none"
 
+from collections.abc import Mapping
+from typing import Any
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
+from cmk.agent_based.v2 import (
+    AgentSection,
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Result,
+    Service,
+    State,
+    StringTable,
+)
 
-check_info = {}
 
-
-def parse_heartbeat_rscstatus(string_table):
+def parse_heartbeat_rscstatus(string_table: StringTable) -> str | None:
     try:
         return string_table[0][0]
     except IndexError:
         return None
 
 
-def discover_heartbeat_rscstatus(heartbeat_rsc_status):
-    if heartbeat_rsc_status is not None:
-        yield None, {"discovered_state": heartbeat_rsc_status}
+def discover_heartbeat_rscstatus(section: str | None) -> DiscoveryResult:
+    if section is not None:
+        yield Service(parameters={"discovered_state": section})
 
 
-def check_heartbeat_rscstatus(_no_item, params, heartbeat_rsc_status):
-    if heartbeat_rsc_status is None:
+def check_heartbeat_rscstatus(params: Mapping[str, Any], section: str | None) -> CheckResult:
+    if section is None:
         return
 
     if not isinstance(params, dict):
-        # old params comes styled with double qoutes
-        params = {"discovered_state": params.replace('"', "")}
+        # old params comes styled with double quotes
+        params = {"discovered_state": str(params).replace('"', "")}
 
     expected_state = params.get("discovered_state")
     if "expected_state" in params:
         expected_state = params["expected_state"]
 
-    if expected_state == heartbeat_rsc_status:
-        yield 0, "Current state: %s" % heartbeat_rsc_status
+    if expected_state == section:
+        yield Result(state=State.OK, summary=f"Current state: {section}")
     else:
-        yield 2, f"Current state: {heartbeat_rsc_status} (Expected: {expected_state})"
+        yield Result(
+            state=State.CRIT,
+            summary=f"Current state: {section} (Expected: {expected_state})",
+        )
 
 
-check_info["heartbeat_rscstatus"] = LegacyCheckDefinition(
+agent_section_heartbeat_rscstatus = AgentSection(
     name="heartbeat_rscstatus",
     parse_function=parse_heartbeat_rscstatus,
+)
+
+check_plugin_heartbeat_rscstatus = CheckPlugin(
+    name="heartbeat_rscstatus",
     service_name="Heartbeat Ressource Status",
     discovery_function=discover_heartbeat_rscstatus,
     check_function=check_heartbeat_rscstatus,
     check_ruleset_name="heartbeat_rscstatus",
+    check_default_parameters={},
 )
--- tests/unit/cmk/legacy_checks/test_heartbeat_rscstatus.py
+++ tests/unit/cmk/legacy_checks/test_heartbeat_rscstatus.py
@@ -3,15 +3,11 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="misc"
-# mypy: disable-error-code="no-untyped-call"
-
 from collections.abc import Mapping, Sequence
-from typing import Any
 
 import pytest
 
-from cmk.agent_based.v2 import StringTable
+from cmk.agent_based.v2 import Result, Service, State, StringTable
 from cmk.legacy_checks.heartbeat_rscstatus import (
     check_heartbeat_rscstatus,
     discover_heartbeat_rscstatus,
@@ -22,36 +18,39 @@ from cmk.legacy_checks.heartbeat_rscstatus import (
 @pytest.mark.parametrize(
     "string_table, expected_discoveries",
     [
-        ([["all"]], [(None, {"discovered_state": "all"})]),
+        ([["all"]], [Service(parameters={"discovered_state": "all"})]),
     ],
 )
 def test_discover_heartbeat_rscstatus(
-    string_table: StringTable, expected_discoveries: Sequence[tuple[str, Mapping[str, Any]]]
+    string_table: StringTable, expected_discoveries: Sequence[Service]
 ) -> None:
     """Test discovery function for heartbeat_rscstatus check."""
     parsed = parse_heartbeat_rscstatus(string_table)
     result = list(discover_heartbeat_rscstatus(parsed))
-    assert sorted(result) == sorted(expected_discoveries)
+    assert result == expected_discoveries
 
 
 @pytest.mark.parametrize(
-    "item, params, string_table, expected_results",
+    "params, string_table, expected_results",
     [
-        (None, {"discovered_state": "all"}, [["all"]], [(0, "Current state: all")]),
         (
-            None,
+            {"discovered_state": "all"},
+            [["all"]],
+            [Result(state=State.OK, summary="Current state: all")],
+        ),
+        (
             {"discovered_state": "local"},
             [["all"]],
-            [(2, "Current state: all (Expected: local)")],
+            [Result(state=State.CRIT, summary="Current state: all (Expected: local)")],
         ),
-        (None, '"all"', [["all"]], [(0, "Current state: all")]),
-        (None, '"local"', [["all"]], [(2, "Current state: all (Expected: local)")]),
     ],
 )
 def test_check_heartbeat_rscstatus(
-    item: str, params: Mapping[str, Any], string_table: StringTable, expected_results: Sequence[Any]
+    params: Mapping[str, str],
+    string_table: StringTable,
+    expected_results: Sequence[Result],
 ) -> None:
     """Test check function for heartbeat_rscstatus check."""
     parsed = parse_heartbeat_rscstatus(string_table)
-    result = list(check_heartbeat_rscstatus(item, params, parsed))
+    result = list(check_heartbeat_rscstatus(params, parsed))
     assert result == expected_results
