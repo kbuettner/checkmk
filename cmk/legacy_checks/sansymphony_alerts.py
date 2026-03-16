9dd01ab834d384d97098739cd9d47529df57bfe8
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-03-16 21:28:49 +0100

    Legacy check migration: sansymphony_alerts I
    Change-Id: I87c294fc70b07ed9ed34ecfcf854ccb82bb60760

--- cmk/legacy_checks/sansymphony_alerts.py
+++ cmk/legacy_checks/sansymphony_alerts.py
@@ -3,37 +3,49 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
 
+from collections.abc import Mapping
+from typing import Any
 
-from cmk.agent_based.legacy.v0_unstable import check_levels, LegacyCheckDefinition
-from cmk.agent_based.v2 import StringTable
-
-check_info = {}
-
-
-def discover_sansymphony_alerts(info):
-    yield None, {}
-
-
-def check_sansymphony_alerts(_no_item, params, info):
-    nr_of_alerts = int(info[0][0])
-    yield check_levels(
-        nr_of_alerts,
-        "alerts",
-        params["levels"],
-        human_readable_func=str,
-        infoname="Unacknowlegded alerts",
-    )
+from cmk.agent_based.v2 import (
+    AgentSection,
+    check_levels,
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Service,
+    StringTable,
+)
 
 
 def parse_sansymphony_alerts(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["sansymphony_alerts"] = LegacyCheckDefinition(
+agent_section_sansymphony_alerts = AgentSection(
     name="sansymphony_alerts",
     parse_function=parse_sansymphony_alerts,
+)
+
+
+def discover_sansymphony_alerts(section: StringTable) -> DiscoveryResult:
+    yield Service()
+
+
+def check_sansymphony_alerts(params: Mapping[str, Any], section: StringTable) -> CheckResult:
+    nr_of_alerts = int(section[0][0])
+    warn, crit = params["levels"]
+    yield from check_levels(
+        nr_of_alerts,
+        levels_upper=("fixed", (warn, crit)),
+        metric_name="alerts",
+        render_func=str,
+        label="Unacknowlegded alerts",
+    )
+
+
+check_plugin_sansymphony_alerts = CheckPlugin(
+    name="sansymphony_alerts",
     service_name="sansymphony Alerts",
     discovery_function=discover_sansymphony_alerts,
     check_function=check_sansymphony_alerts,
