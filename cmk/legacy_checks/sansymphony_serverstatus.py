d26a719f810b1e2a9078f1cf122729b158ffbe63
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-03-16 21:28:49 +0100

    Legacy check migration: sansymphony_serverstatus I
    Change-Id: Ieaef72d5565eb895a2842c1cc3ad3b669bd4cbf2

--- cmk/legacy_checks/sansymphony_serverstatus.py
+++ cmk/legacy_checks/sansymphony_serverstatus.py
@@ -3,42 +3,57 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
 # <<<sansymphony_serverstatus>>>
 # Online WritebackGlobal
 
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import StringTable
-
-check_info = {}
-
-
-def discover_sansymphony_serverstatus(info):
-    if info:
-        return [(None, None)]
-    return []
-
-
-def check_sansymphony_serverstatus(_no_item, _no_params, info):
-    if not info:
-        return None
-    status, cachestate = info[0]
-    if status == "Online" and cachestate == "WritebackGlobal":
-        return 0, f"SANsymphony is {status} and its cache is in {cachestate} mode"
-    if status == "Online" and cachestate != "WritebackGlobal":
-        return 1, f"SANsymphony is {status} but its cache is in {cachestate} mode"
-    return 2, "SANsymphony is %s" % status
+from cmk.agent_based.v2 import (
+    AgentSection,
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Result,
+    Service,
+    State,
+    StringTable,
+)
 
 
 def parse_sansymphony_serverstatus(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["sansymphony_serverstatus"] = LegacyCheckDefinition(
+agent_section_sansymphony_serverstatus = AgentSection(
     name="sansymphony_serverstatus",
     parse_function=parse_sansymphony_serverstatus,
+)
+
+
+def discover_sansymphony_serverstatus(section: StringTable) -> DiscoveryResult:
+    if section:
+        yield Service()
+
+
+def check_sansymphony_serverstatus(section: StringTable) -> CheckResult:
+    if not section:
+        return
+    status, cachestate = section[0]
+    if status == "Online" and cachestate == "WritebackGlobal":
+        yield Result(
+            state=State.OK,
+            summary=f"SANsymphony is {status} and its cache is in {cachestate} mode",
+        )
+    elif status == "Online" and cachestate != "WritebackGlobal":
+        yield Result(
+            state=State.WARN,
+            summary=f"SANsymphony is {status} but its cache is in {cachestate} mode",
+        )
+    else:
+        yield Result(state=State.CRIT, summary=f"SANsymphony is {status}")
+
+
+check_plugin_sansymphony_serverstatus = CheckPlugin(
+    name="sansymphony_serverstatus",
     service_name="sansymphony Serverstatus",
     discovery_function=discover_sansymphony_serverstatus,
     check_function=check_sansymphony_serverstatus,
