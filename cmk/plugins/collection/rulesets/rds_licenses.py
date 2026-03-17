386c6b04f43beb61a1c6aaa4685d5eac45f3d127
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-03-17 15:44:01 +0100

    bakery: Move rds_licenses rulespec to cmk/plugins/collection/rulesets/
    Move from legacy auto-discovery location to standard plugin discovery
    path at cmk/plugins/collection/rulesets/rds_licenses.py.
    
    Change-Id: I72c4ff95d81286f70a02534d9f3259e7d7828d4b

---
+++ cmk/plugins/collection/rulesets/rds_licenses.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# Copyright (C) 2019 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from collections.abc import Mapping
+
+from cmk.rulesets.v1 import Help, Title
+from cmk.rulesets.v1.form_specs import (
+    CascadingSingleChoice,
+    CascadingSingleChoiceElement,
+    DefaultValue,
+    DictElement,
+    Dictionary,
+    FixedValue,
+    TimeMagnitude,
+    TimeSpan,
+)
+from cmk.rulesets.v1.rule_specs import AgentConfig, Topic
+
+
+def _migrate(value: object) -> Mapping[str, object]:
+    if isinstance(value, dict):
+        return value
+    return {"deployment": ("sync" if value else "do_not_deploy", None)}
+
+
+def _valuespec_agent_config_rds_licenses() -> Dictionary:
+    return Dictionary(
+        help_text=Help("Deploy the agent plug-in <tt>rds_licenses</tt> on your target system."),
+        elements={
+            "deployment": DictElement(
+                required=True,
+                parameter_form=CascadingSingleChoice(
+                    title=Title("Deployment type"),
+                    elements=(
+                        CascadingSingleChoiceElement(
+                            name="sync",
+                            title=Title("Deploy the plug-in and run it synchronously"),
+                            parameter_form=FixedValue(value=None),
+                        ),
+                        CascadingSingleChoiceElement(
+                            name="cached",
+                            title=Title("Deploy the plug-in and run it asynchronously"),
+                            parameter_form=TimeSpan(
+                                displayed_magnitudes=(
+                                    TimeMagnitude.HOUR,
+                                    TimeMagnitude.MINUTE,
+                                )
+                            ),
+                        ),
+                        CascadingSingleChoiceElement(
+                            name="do_not_deploy",
+                            title=Title("Do not deploy the plug-in"),
+                            parameter_form=FixedValue(value=None),
+                        ),
+                    ),
+                    prefill=DefaultValue("sync"),
+                ),
+            ),
+        },
+        migrate=_migrate,
+    )
+
+
+rule_spec_rds_licenses = AgentConfig(
+    title=Title("Remote Desktop Licenses (Windows)"),
+    name="rds_licenses",
+    topic=Topic.APPLICATIONS,
+    parameter_form=_valuespec_agent_config_rds_licenses,
+)
