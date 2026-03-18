49e1ad6f6092ec7bdbc92ff7d56547054e1634bc
Andreas Hasenkopf <andreas.hasenkopf@checkmk.com>
2026-03-18 13:55:41 +0100

    Legacy check migration: dell_chassis_temp I
    Change-Id: Ic735ac4ec4f1dc3d7ec3981c3a7e1f1886ef4786

--- cmk/legacy_checks/dell_chassis_temp.py
+++ cmk/legacy_checks/dell_chassis_temp.py
@@ -3,52 +3,66 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
 
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
-from cmk.legacy_includes.temperature import check_temperature
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    get_value_store,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 from cmk.plugins.dell.lib import DETECT_CHASSIS
+from cmk.plugins.lib.temperature import check_temperature, TempParamType
 
-check_info = {}
-
-
-def discover_dell_chassis_temp(info):
-    if info and len(info[0]) == 3:
-        yield "Front Panel", {}
-        yield "CMC Ambient", {}
-        yield "CMC Processor", {}
+_ITEMS = {
+    "Front Panel": 0,
+    "CMC Ambient": 1,
+    "CMC Processor": 2,
+}
 
 
-def check_dell_chassis_temp(item, params, info):
-    items = {
-        "Front Panel": 0,
-        "CMC Ambient": 1,
-        "CMC Processor": 2,
-    }
+def discover_dell_chassis_temp(section: StringTable) -> DiscoveryResult:
+    if section and len(section[0]) == 3:
+        yield Service(item="Front Panel")
+        yield Service(item="CMC Ambient")
+        yield Service(item="CMC Processor")
 
-    if item in items:
-        item_id = items[item]
 
-        temp = float(info[0][item_id])
-        return check_temperature(temp, params, "dell_chassis_temp_%s" % item)
+def check_dell_chassis_temp(item: str, params: TempParamType, section: StringTable) -> CheckResult:
+    if item in _ITEMS:
+        yield from check_temperature(
+            reading=float(section[0][_ITEMS[item]]),
+            params=params,
+            unique_name=f"dell_chassis_temp_{item}",
+            value_store=get_value_store(),
+        )
+        return
 
-    return 3, "Sensor not found in SNMP data"
+    yield Result(state=State.UNKNOWN, summary="Sensor not found in SNMP data")
 
 
 def parse_dell_chassis_temp(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["dell_chassis_temp"] = LegacyCheckDefinition(
+snmp_section_dell_chassis_temp = SimpleSNMPSection(
     name="dell_chassis_temp",
-    parse_function=parse_dell_chassis_temp,
     detect=DETECT_CHASSIS,
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.674.10892.2.3.1",
         oids=["10", "11", "12"],
     ),
+    parse_function=parse_dell_chassis_temp,
+)
+
+
+check_plugin_dell_chassis_temp = CheckPlugin(
+    name="dell_chassis_temp",
     service_name="Temperature %s",
     discovery_function=discover_dell_chassis_temp,
     check_function=check_dell_chassis_temp,
