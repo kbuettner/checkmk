9457b1c467de0c14fb0b7f5d78b666bed4e805b6
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-03-19 22:14:38 +0100

    Legacy check migration: atto_fibrebridge_fcport I
    Change-Id: Ieb0be9f45c35e669ea0c8e1f5d84515145a05fb7

--- cmk/legacy_checks/atto_fibrebridge_fcport.py
+++ cmk/legacy_checks/atto_fibrebridge_fcport.py
@@ -3,46 +3,35 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-# mypy: disable-error-code="possibly-undefined"
-
 
 import time
+from collections.abc import Mapping
+from typing import Any
 
-from cmk.agent_based.legacy.v0_unstable import check_levels, LegacyCheckDefinition
-from cmk.agent_based.v2 import get_rate, get_value_store, OIDEnd, SNMPTree, startswith, StringTable
-
-check_info = {}
-
-
-def discover_atto_fibrebridge_fcport(info):
-    for line in info:
-        yield line[0], {}
-
-
-def check_atto_fibrebridge_fcport(item, params, info):
-    now = time.time()
-    for line in info:
-        if line[0] == item:
-            fc_tx_words = get_rate(get_value_store(), "tx", now, int(line[1]), raise_overflow=True)
-            fc_rx_words = get_rate(get_value_store(), "rx", now, int(line[2]), raise_overflow=True)
-
-    if not params["fc_tx_words"]:
-        yield 0, "TX: %.2f words/s" % fc_tx_words, [("fc_tx_words", fc_tx_words)]
-    else:
-        yield check_levels(fc_tx_words, "fc_tx_words", params["fc_tx_words"], infoname="TX")
-
-    if not params["fc_rx_words"]:
-        yield 0, "RX: %.2f words/s" % fc_rx_words, [("fc_rx_words", fc_rx_words)]
-    else:
-        yield check_levels(fc_rx_words, "fc_rx_words", params["fc_rx_words"], infoname="RX")
+from cmk.agent_based.v2 import (
+    check_levels,
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    get_rate,
+    get_value_store,
+    Metric,
+    OIDEnd,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    startswith,
+    State,
+    StringTable,
+)
 
 
 def parse_atto_fibrebridge_fcport(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["atto_fibrebridge_fcport"] = LegacyCheckDefinition(
+snmp_section_atto_fibrebridge_fcport = SimpleSNMPSection(
     name="atto_fibrebridge_fcport",
     parse_function=parse_atto_fibrebridge_fcport,
     detect=startswith(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.4547"),
@@ -50,6 +39,53 @@ check_info["atto_fibrebridge_fcport"] = LegacyCheckDefinition(
         base=".1.3.6.1.4.1.4547.2.3.3.2.1",
         oids=[OIDEnd(), "2", "3"],
     ),
+)
+
+
+def discover_atto_fibrebridge_fcport(section: StringTable) -> DiscoveryResult:
+    for line in section:
+        yield Service(item=line[0])
+
+
+def check_atto_fibrebridge_fcport(
+    item: str, params: Mapping[str, Any], section: StringTable
+) -> CheckResult:
+    now = time.time()
+    fc_tx_words: float | None = None
+    fc_rx_words: float | None = None
+    for line in section:
+        if line[0] == item:
+            fc_tx_words = get_rate(get_value_store(), "tx", now, int(line[1]), raise_overflow=True)
+            fc_rx_words = get_rate(get_value_store(), "rx", now, int(line[2]), raise_overflow=True)
+
+    if fc_tx_words is None or fc_rx_words is None:
+        return
+
+    if not params["fc_tx_words"]:
+        yield Result(state=State.OK, summary=f"TX: {fc_tx_words:.2f} words/s")
+        yield Metric("fc_tx_words", fc_tx_words)
+    else:
+        yield from check_levels(
+            fc_tx_words,
+            metric_name="fc_tx_words",
+            levels_upper=("fixed", params["fc_tx_words"]),
+            label="TX",
+        )
+
+    if not params["fc_rx_words"]:
+        yield Result(state=State.OK, summary=f"RX: {fc_rx_words:.2f} words/s")
+        yield Metric("fc_rx_words", fc_rx_words)
+    else:
+        yield from check_levels(
+            fc_rx_words,
+            metric_name="fc_rx_words",
+            levels_upper=("fixed", params["fc_rx_words"]),
+            label="RX",
+        )
+
+
+check_plugin_atto_fibrebridge_fcport = CheckPlugin(
+    name="atto_fibrebridge_fcport",
     service_name="FC Port %s",
     discovery_function=discover_atto_fibrebridge_fcport,
     check_function=check_atto_fibrebridge_fcport,
