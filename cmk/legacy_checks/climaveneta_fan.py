60e2cffafd898a1d34a1f85435d12223b08d99af
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-03-19 22:20:00 +0100

    Legacy check migration: climaveneta_fan I
    Change-Id: I8b7aeb03eb434c9c2dd3106e04a923ed362b902e

--- cmk/legacy_checks/climaveneta_fan.py
+++ cmk/legacy_checks/climaveneta_fan.py
@@ -3,14 +3,36 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
+
+from collections.abc import Mapping
+from typing import Any
+
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    equals,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    StringTable,
+)
+from cmk.plugins.lib.fan import check_fan
 
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import DiscoveryResult, equals, Service, SNMPTree, StringTable
-from cmk.legacy_includes.fan import check_fan
+def parse_climaveneta_fan(string_table: StringTable) -> StringTable:
+    return string_table
 
-check_info = {}
+
+snmp_section_climaveneta_fan = SimpleSNMPSection(
+    name="climaveneta_fan",
+    parse_function=parse_climaveneta_fan,
+    detect=equals(".1.3.6.1.2.1.1.1.0", "pCO Gateway"),
+    fetch=SNMPTree(
+        base=".1.3.6.1.4.1.9839.2.1.2",
+        oids=["42", "43"],
+    ),
+)
 
 
 def discover_climaveneta_fan(section: StringTable) -> DiscoveryResult:
@@ -19,23 +41,15 @@ def discover_climaveneta_fan(section: StringTable) -> DiscoveryResult:
         yield Service(item="2")
 
 
-def check_climaveneta_fan(item, params, info):
-    rpm = int(info[0][int(item) - 1])
-    return check_fan(rpm, params)
+def check_climaveneta_fan(
+    item: str, params: Mapping[str, Any], section: StringTable
+) -> CheckResult:
+    rpm = int(section[0][int(item) - 1])
+    yield from check_fan(rpm, params)
 
 
-def parse_climaveneta_fan(string_table: StringTable) -> StringTable:
-    return string_table
-
-
-check_info["climaveneta_fan"] = LegacyCheckDefinition(
+check_plugin_climaveneta_fan = CheckPlugin(
     name="climaveneta_fan",
-    parse_function=parse_climaveneta_fan,
-    detect=equals(".1.3.6.1.2.1.1.1.0", "pCO Gateway"),
-    fetch=SNMPTree(
-        base=".1.3.6.1.4.1.9839.2.1.2",
-        oids=["42", "43"],
-    ),
     service_name="Fan %s",
     discovery_function=discover_climaveneta_fan,
     check_function=check_climaveneta_fan,
