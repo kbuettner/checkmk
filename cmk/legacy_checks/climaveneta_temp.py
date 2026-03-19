e9c50528bdec0395ae406c43497053d1a804ebca
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-03-19 22:20:27 +0100

    Legacy check migration: climaveneta_temp I
    Change-Id: Ia3ae3cb6a60f16d98857ef8db26b27ea6be668e9

--- cmk/legacy_checks/climaveneta_temp.py
+++ cmk/legacy_checks/climaveneta_temp.py
@@ -3,16 +3,24 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
 
+from collections.abc import Mapping
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import equals, OIDEnd, SNMPTree, StringTable
-from cmk.legacy_includes.temperature import check_temperature
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    equals,
+    get_value_store,
+    OIDEnd,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    StringTable,
+)
+from cmk.plugins.lib.temperature import check_temperature, TempParamType
 
-check_info = {}
-
-climaveneta_sensors = {
+climaveneta_sensors: Mapping[int, str] = {
     1: "Room",
     3: "Outlet Air 1",
     4: "Outlet Air 2",
@@ -33,27 +41,11 @@ climaveneta_sensors = {
 }
 
 
-def discover_climaveneta_temp(info):
-    for sensor_id, value in info:
-        sensor_id = int(sensor_id.split(".")[0])
-        if sensor_id in climaveneta_sensors and int(value) > 0:
-            yield climaveneta_sensors[sensor_id], {}
-
-
-def check_climaveneta_temp(item, params, info):
-    for sensor_id, sensor_value in info:
-        sensor_id = int(sensor_id.split(".")[0])
-        if climaveneta_sensors.get(sensor_id) == item:
-            sensor_value = int(sensor_value) / 10.0
-            return check_temperature(sensor_value, params, "climaveneta_temp_%s" % item)
-    return None
-
-
 def parse_climaveneta_temp(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["climaveneta_temp"] = LegacyCheckDefinition(
+snmp_section_climaveneta_temp = SimpleSNMPSection(
     name="climaveneta_temp",
     parse_function=parse_climaveneta_temp,
     detect=equals(".1.3.6.1.2.1.1.1.0", "pCO Gateway"),
@@ -61,6 +53,31 @@ check_info["climaveneta_temp"] = LegacyCheckDefinition(
         base=".1.3.6.1.4.1.9839.2.1",
         oids=[OIDEnd(), "2"],
     ),
+)
+
+
+def discover_climaveneta_temp(section: StringTable) -> DiscoveryResult:
+    for sensor_id, value in section:
+        sensor_id_int = int(sensor_id.split(".")[0])
+        if sensor_id_int in climaveneta_sensors and int(value) > 0:
+            yield Service(item=climaveneta_sensors[sensor_id_int])
+
+
+def check_climaveneta_temp(item: str, params: TempParamType, section: StringTable) -> CheckResult:
+    for sensor_id, sensor_value in section:
+        sensor_id_int = int(sensor_id.split(".")[0])
+        if climaveneta_sensors.get(sensor_id_int) == item:
+            yield from check_temperature(
+                reading=int(sensor_value) / 10.0,
+                params=params,
+                unique_name=f"climaveneta_temp_{item}",
+                value_store=get_value_store(),
+            )
+            return
+
+
+check_plugin_climaveneta_temp = CheckPlugin(
+    name="climaveneta_temp",
     service_name="Temperature %s",
     discovery_function=discover_climaveneta_temp,
     check_function=check_climaveneta_temp,
