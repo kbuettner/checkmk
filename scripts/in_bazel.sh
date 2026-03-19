7a5a10f8bebd07d2638a3aa044058dcad14f7368
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-03-19 13:06:42 +0100

    in_basel.sh: make exceptions less convenient
    This is a compromise: We do want to have this feature
    to provide the possibility of short term exceptions
    without disabling the job altogether, while at the
    same time adding such an exception should not be
    advertised or convenient.
    
    CMK-32476
    
    Change-Id: I88a23ac20dd63762ad9f4cd21c892b7c8722f2a0

--- scripts/in_bazel.sh
+++ scripts/in_bazel.sh
@@ -13,26 +13,44 @@ current_offenders() {
         )
 }
 
+known_offenders() {
+    # *As a last resort*, you can temporarily pass known offenders as arguments.
+    # This is deliberately inconvenient and undocumented as it is highly discouraged.
+    # Rather than adding something here, you should put these files into an appropriate
+    # bazel target.
+    while [ "$1" == "--known-offender" ]; do
+        echo "$2"
+        shift 2
+    done
+}
+
 main() {
-    if [[ $# -ne 2 && $# -ne 3 ]]; then
-        echo "Usage: $0 <EXTENSION> <KIND> [<LIMIT>]" >&2
+    if [[ $# -lt 2 ]]; then
+        echo "Usage: $0 <EXTENSION> <KIND>" >&2
         exit 1
     fi
 
-    ext=$1
-    kind=$2
-    limit=${3:-0}
+    ext="$1"
+    kind="$2"
+    shift 2
 
     current=$(current_offenders)
+    expected=$(known_offenders "$@" | sort)
 
-    count=$(echo "$current" | wc -l)
-    echo "Found $count \".$ext\" files not declared as $kind, expected exactly $limit"
-    echo "-----"
-    echo "$current"
+    fixed=$(comm -13 <(echo "${current}") <(echo "${expected}"))
+    broken=$(comm -23 <(echo "${current}") <(echo "${expected}"))
 
-    if ((count != limit)); then
-        exit 1
+    if [ -n "${fixed}" ]; then
+        echo "Found \".${ext}\" files declared as ${kind} but listed as exception:"
+        echo "${fixed}"
     fi
+
+    if [ -n "${broken}" ]; then
+        echo "Found \".${ext}\" files not declared as ${kind}:"
+        echo "${broken}"
+    fi
+
+    [ -z "${fixed}${broken}" ]
 }
 
 main "$@"
