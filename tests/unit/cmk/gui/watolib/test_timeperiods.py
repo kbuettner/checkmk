e7ee7745395efb67e642d680e2a88c02f6e5950f
Lars Michelsen <lm@checkmk.com>
2026-03-20 23:04:57 +0100

    Split up edition sensitive test into multiple conditional test cases #5
    These tests can later be executed separately by the individual edition
    test targets without conditional.
    
    CMK-32598
    
    Change-Id: I65133a06e9f6920a7a721f5c06d93124f8e47c08

--- tests/unit/cmk/gui/watolib/test_timeperiods.py
+++ tests/unit/cmk/gui/watolib/test_timeperiods.py
@@ -3,10 +3,17 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+import pytest
+
+from cmk.ccc.version import Edition, edition
 from cmk.gui.watolib.timeperiods import timeperiod_usage_finder_registry
-from tests.testlib.common.repo import is_pro_repo
+from cmk.utils import paths
 
 
+@pytest.mark.skipif(
+    edition(paths.omd_root) is not Edition.COMMUNITY,
+    reason="Remove condition with CMK-32598",
+)
 def test_group_usage_finder_registry_entries() -> None:
     expected = [
         "find_timeperiod_usage_in_ec_rules",
@@ -15,9 +22,6 @@ def test_group_usage_finder_registry_entries() -> None:
         "find_timeperiod_usage_in_time_specific_parameters",
         "find_timeperiod_usage_in_users",
     ]
-
-    if is_pro_repo():
-        expected.append("find_timeperiod_usage_in_alert_handler_rules")
 
     registered = [f.__name__ for f in timeperiod_usage_finder_registry.values()]
     assert sorted(registered) == sorted(expected)
