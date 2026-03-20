2f88793cbab1e0ed7d22bf06bd06975dc20aa58b
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-03-20 23:44:19 +0100

    Legacy check migration: ipr400_temp I
    Change-Id: Ib84cba63563e08c3de97b9963e7e5591a71fd868

--- cmk/legacy_checks/ipr400_temp.py
+++ cmk/legacy_checks/ipr400_temp.py
@@ -4,31 +4,39 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 
-from cmk.agent_based.legacy.v0_unstable import (
-    LegacyCheckDefinition,
-    LegacyCheckResult,
-    LegacyDiscoveryResult,
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    get_value_store,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    startswith,
+    StringTable,
 )
-from cmk.agent_based.v2 import SNMPTree, startswith, StringTable
-from cmk.legacy_includes.temperature import check_temperature, TempParamType
-
-check_info = {}
+from cmk.plugins.lib.temperature import check_temperature, TempParamType
 
 
-def discover_ipr400_temp(info: StringTable) -> LegacyDiscoveryResult:
-    if len(info) > 0:
-        yield "Ambient", {}
+def discover_ipr400_temp(section: StringTable) -> DiscoveryResult:
+    if len(section) > 0:
+        yield Service(item="Ambient")
 
 
-def check_ipr400_temp(item: str, params: TempParamType, info: StringTable) -> LegacyCheckResult:
-    yield check_temperature(int(info[0][0]), params, "ipr400_temp_%s" % item)
+def check_ipr400_temp(item: str, params: TempParamType, section: StringTable) -> CheckResult:
+    yield from check_temperature(
+        reading=int(section[0][0]),
+        params=params,
+        unique_name=f"ipr400_temp_{item}",
+        value_store=get_value_store(),
+    )
 
 
 def parse_ipr400_temp(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["ipr400_temp"] = LegacyCheckDefinition(
+snmp_section_ipr400_temp = SimpleSNMPSection(
     name="ipr400_temp",
     parse_function=parse_ipr400_temp,
     detect=startswith(".1.3.6.1.2.1.1.1.0", "ipr voip device ipr400"),
@@ -36,6 +44,10 @@ check_info["ipr400_temp"] = LegacyCheckDefinition(
         base=".1.3.6.1.4.1.27053.1.4.5",
         oids=["9"],
     ),
+)
+
+check_plugin_ipr400_temp = CheckPlugin(
+    name="ipr400_temp",
     service_name="Temperature %s ",
     discovery_function=discover_ipr400_temp,
     check_function=check_ipr400_temp,
