25fc25b61cb374d4daf39bc1759a01d12ac9501d
Lars Michelsen <lm@checkmk.com>
2026-03-20 22:57:23 +0100

    Split up edition sensitive test into multiple conditional test cases #3
    These tests can later be executed separately by the individual edition
    test targets without conditional.
    
    CMK-32598
    
    Change-Id: Ie14608ad6d182d5c76c56a9d7ed1e74b4bf05ad3

--- tests/unit/cmk/gui/test_pagetypes.py
+++ tests/unit/cmk/gui/test_pagetypes.py
@@ -3,11 +3,18 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+import pytest
+
+from cmk.ccc.version import Edition, edition
 from cmk.gui.pagetypes import PagetypeTopics
 from cmk.gui.utils.script_helpers import application_and_request_context
-from tests.testlib.common.repo import is_pro_repo
+from cmk.utils import paths
 
 
+@pytest.mark.skipif(
+    edition(paths.omd_root) is not Edition.COMMUNITY,
+    reason="Remove condition with CMK-32598",
+)
 def test_registered_pagetype_topics() -> None:
     expected = [
         "analyze",
@@ -25,9 +32,6 @@ def test_registered_pagetype_topics() -> None:
         "problems",
         "synthetic_monitoring",
     ]
-
-    if is_pro_repo():
-        expected.append("network_statistics")
 
     with application_and_request_context():
         assert sorted(list(PagetypeTopics.builtin_pages().keys())) == sorted(expected)
