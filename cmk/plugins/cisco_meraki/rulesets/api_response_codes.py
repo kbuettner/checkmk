d10f3b71bdd364343d047135e7efce964168795e
Logan Connolly <logan.connolly@checkmk.com>
2026-03-25 10:16:38 +0100

    19549 cisco_meraki: fix API response codes title
    CMK-32895
    
    Change-Id: I9720720168ebe9797fc15d6aaa8deffa134db507

--- cmk/plugins/cisco_meraki/rulesets/api_response_codes.py
+++ cmk/plugins/cisco_meraki/rulesets/api_response_codes.py
@@ -32,6 +32,6 @@ rule_spec_cisco_meraki_org_api_response_codes = CheckParameters(
     name="cisco_meraki_org_api_response_codes",
     topic=Topic.NETWORKING,
     parameter_form=_parameter_form,
-    title=Title("Cisco Meraki appliance performance utilization"),
+    title=Title("Cisco Meraki API response codes"),
     condition=HostAndItemCondition(item_title=Title("Organization")),
 )
