0046326c3d8c4ed3e331dfc765f04d1c2915e57b
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-03-25 20:31:46 +0100

    edition tests: first example post_rename_site
    This moves tests that test the feature set of
    an edition into a dedicated test category.
    For now we keep them with the "unit tests".
    This way they are run as part of the CV.
    I acknowledg they are not unit tests.
    
    Change-Id: I631eebdc145e33502dc37a4ca0e5ead4c089824f

--- tests/unit/editions/community/test_plugins_post_rename_site.py
+++ tests/unit/editions/community/test_plugins_post_rename_site.py
@@ -3,20 +3,10 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-import pytest
-
-from cmk.ccc.version import Edition, edition
 from cmk.post_rename_site import main
-from cmk.utils import paths
 
 
-@pytest.mark.skipif(
-    edition(paths.omd_root) is not Edition.COMMUNITY,
-    reason="Remove condition with CMK-32598",
-)
 def test_load_plugins() -> None:
-    """The test changes a global variable `rename_action_registry`.
-    We can't reliably monkey patch this variable - must use separate module for testing"""
     assert {p.name for p in main.load_plugins()} == {
         "sites",
         "messaging",
