381e1248f8b8648b94f394d29e357b53427353cf
Logan Connolly <logan.connolly@checkmk.com>
2026-03-25 09:36:56 +0100

    cisco_meraki: fix naming inconsistencies in rules
    Change-Id: I8e8229af74b29ce7ca79e054d3f3f5712f00a344

--- cmk/plugins/cisco_meraki/rulesets/api_response_codes.py
+++ cmk/plugins/cisco_meraki/rulesets/api_response_codes.py
@@ -28,7 +28,7 @@ def _parameter_form() -> Dictionary:
     )
 
 
-rule_spec_cisco_meraki_org_device_status_ps = CheckParameters(
+rule_spec_cisco_meraki_org_api_response_codes = CheckParameters(
     name="cisco_meraki_org_api_response_codes",
     topic=Topic.NETWORKING,
     parameter_form=_parameter_form,
