fa0db7af72874f4207523f9a094d56b315cd0f0b
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-03-25 16:29:21 +0100

    improve formatting
    Change-Id: Ia3a51457fa12d6c4fe0cda85bcd9464d2cb3451a

--- packages/cmk-plugin-apis/cmk/discover_plugins/__init__.py
+++ packages/cmk-plugin-apis/cmk/discover_plugins/__init__.py
@@ -17,51 +17,19 @@ of plugins developed against a versionized API.
 Please keep this in mind when trying to consolidate.
 """
 
-from ._libexec import (
-    discover_executable as discover_executable,
-)
-from ._libexec import (
-    family_libexec_dir as family_libexec_dir,
-)
-from ._python_plugins import (
-    addons_plugins_local_path as addons_plugins_local_path,
-)
-from ._python_plugins import (
-    Collector as Collector,
-)
-from ._python_plugins import (
-    discover_all_plugins as discover_all_plugins,
-)
-from ._python_plugins import (
-    discover_families as discover_families,
-)
-from ._python_plugins import (
-    discover_modules as discover_modules,
-)
-from ._python_plugins import (
-    discover_plugins_from_modules as discover_plugins_from_modules,
-)
-from ._python_plugins import (
-    discover_submodules as discover_submodules,
-)
-from ._python_plugins import (
-    DiscoveredPlugins as DiscoveredPlugins,
-)
-from ._python_plugins import (
-    PluginLocation as PluginLocation,
-)
-from ._python_plugins import (
-    plugins_local_path as plugins_local_path,
-)
-from ._wellknown import (
-    AGENT_PLUGINS_FOLDER as AGENT_PLUGINS_FOLDER,
-)
-from ._wellknown import (
-    CMK_ADDONS_PLUGINS as CMK_ADDONS_PLUGINS,
-)
-from ._wellknown import (
-    CMK_PLUGINS as CMK_PLUGINS,
-)
-from ._wellknown import (
-    PluginGroup as PluginGroup,
-)
+from ._libexec import discover_executable as discover_executable
+from ._libexec import family_libexec_dir as family_libexec_dir
+from ._python_plugins import addons_plugins_local_path as addons_plugins_local_path
+from ._python_plugins import Collector as Collector
+from ._python_plugins import discover_all_plugins as discover_all_plugins
+from ._python_plugins import discover_families as discover_families
+from ._python_plugins import discover_modules as discover_modules
+from ._python_plugins import discover_plugins_from_modules as discover_plugins_from_modules
+from ._python_plugins import discover_submodules as discover_submodules
+from ._python_plugins import DiscoveredPlugins as DiscoveredPlugins
+from ._python_plugins import PluginLocation as PluginLocation
+from ._python_plugins import plugins_local_path as plugins_local_path
+from ._wellknown import AGENT_PLUGINS_FOLDER as AGENT_PLUGINS_FOLDER
+from ._wellknown import CMK_ADDONS_PLUGINS as CMK_ADDONS_PLUGINS
+from ._wellknown import CMK_PLUGINS as CMK_PLUGINS
+from ._wellknown import PluginGroup as PluginGroup
